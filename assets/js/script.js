7867b0cbeb8ce4275cf75cb816117cf01549ad20
ReduanHalim <>
2023-11-16 19:57:21 +0000

    Adding 10 questions in the js file. Each question have 4 answers to choose from, 1 is correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,92 @@
-console.log("Connected!")
+const questions = [
+    {
+        question: "What is the name of South Koreas largest company?",
+        answers: [
+            { text: "LG", correct: false },
+            { text: "Hyundai", correct: false },
+            { text: "Samsung", correct: true },
+            { text: "Kia", correct: false },
+        ]
+    },
+    {
+        question: "Which animal can be seen on the Porsche logo?",
+        answers: [
+            { text: "Horse", correct: true },
+            { text: "Sheep", correct: false },
+            { text: "Ox", correct: false },
+            { text: "Lion", correct: false },
+        ]
+    },
+    {
+        question: "What is the name of the World's largest ocean?",
+        answers: [
+            { text: "Atlantic ocean", correct: false },
+            { text: "Indian ocean", correct: false },
+            { text: "Pacific ocean", correct: true },
+            { text: "Artic ocean", correct: false },
+        ]
+    },
+    {
+        question: "Which country consumes the most chocolate per capita?",
+        answers: [
+            { text: "Belgium", correct: false },
+            { text: "France", correct: false },
+            { text: "Italy", correct: false },
+            { text: "Switzerland", correct: true },
+        ]
+    },
+    {
+        question: "What is the most consumed manufactured drink in the world?",
+        answers: [
+            { text: "Soda", correct: false },
+            { text: "Coffe", correct: false },
+            { text: "Juice", correct: false },
+            { text: "Tea", correct: true },
+        ]
+    },
+    {
+        question: "What's the fastest land animal?",
+        answers: [
+            { text: "Cheetah", correct: true },
+            { text: "Gazelle", correct: false },
+            { text: "Cougar", correct: false },
+            { text: "Puma", correct: false },
+        ]
+    },
+    {
+        question: "What color is a ruby?",
+        answers: [
+            { text: "Green", correct: false },
+            { text: "Blue", correct: false },
+            { text: "Red", correct: true },
+            { text: "Yellow", correct: false },
+        ]
+    },
+    {
+        question: "What's the smallest country in the world?",
+        answers: [
+            { text: "Luxembourg", correct: false },
+            { text: "Vatican city", correct: true },
+            { text: "Monaco", correct: false },
+            { text: "Andorra", correct: false },
+        ]
+    },
+    {
+        question: "How many hearts does an octopus have?",
+        answers: [
+            { text: "5", correct: false },
+            { text: "3", correct: true },
+            { text: "1", correct: false },
+            { text: "2", correct: false },
+        ]
+    },
+    {
+        question: "What is the nearest planet to the sun?",
+        answers: [
+            { text: "Mars", correct: false },
+            { text: "Jupiter", correct: false },
+            { text: "Saturn", correct: false },
+            { text: "Mercury", correct: true },
+        ]
+    },
+]
