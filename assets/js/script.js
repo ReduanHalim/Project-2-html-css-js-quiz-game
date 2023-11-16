458977f2a5e0add74bf66fb24ee49c2724e0b8a6
ReduanHalim <>
2023-11-16 21:48:38 +0000

    Adding functions in js file. Green backgroundcolor if correct answer, red background color if wrong answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,8 +35,7 @@ const questions = [
             { text: "Switzerland", correct: true },
         ]
     },
-    {
-        question: "What is the most consumed manufactured drink in the world?",
+    {   question: "What is the most consumed manufactured drink in the world?",
         answers: [
             { text: "Soda", correct: false },
             { text: "Coffe", correct: false },
@@ -88,5 +87,65 @@ const questions = [
             { text: "Saturn", correct: false },
             { text: "Mercury", correct: true },
         ]
-    },
-]
+    }
+    
+];
+
+const questionElement = document.getElementById("question"); 
+const answerButtons = document.getElementById("answer-buttons"); 
+const nextButton = document.getElementById("next-btn"); 
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz (){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.
+    question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button =>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabeld = true;
+    });
+    nextButton.style.display = "block";
+}
+
+startQuiz();
